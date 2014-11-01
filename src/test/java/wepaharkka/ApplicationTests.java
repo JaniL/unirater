1def260ecd503561252c16156742cbddc66880cf
Timo Tuononen <tixtixti@Timo-MacBook-Air.local>
2014-11-01 17:05:37 +0200

    Kirjoitin domain testejä

--- src/test/java/wepaharkka/ApplicationTests.java
+++ src/test/java/wepaharkka/ApplicationTests.java
@@ -1,10 +1,13 @@
 package wepaharkka;
 
+import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import wepaharkka.domain.Food;
+import wepaharkka.domain.Price;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = Application.class)
@@ -13,6 +16,11 @@ public class ApplicationTests {
 
 	@Test
 	public void contextLoads() {
+             Food food  = new Food();
+        food.setName("porkkanakakku");
+        food.setPrice(Price.Kevyesti);
+        
+        assertEquals(food.getPrice(), Price.Kevyesti);
 	}
 
 }
