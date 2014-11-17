2beef5e36ad7a6a06e721397073aa33260fde96f
Jani Luukko <jj.luukko@gmail.com>
2014-11-17 12:25:19 +0200

    Staattisen sisällön tarjoaminen fiksattu

--- src/main/java/wepaharkka/config/StaticConfig.java
+++ src/main/java/wepaharkka/config/StaticConfig.java
@@ -1,12 +1,19 @@
 package wepaharkka.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-public class StaticConfig {
-	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+// @ComponentScan("my.root.package")
+@EnableWebMvc
+public class StaticConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		System.out.println("jouaskdhsakjfsaehdfs");
 		  registry.addResourceHandler("/static/**")
 		    .addResourceLocations("classpath:/static/");
-		}
+    }
 }
