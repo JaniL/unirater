f718010afaa81f3f0d4b80e89c76d1868cfe4f2b
neodyymi <ville-veikko.saari@cs.helsinki.fi>
2014-11-19 01:56:11 +0200

    devprofilesta init kommentoitu pois

--- src/main/java/unicaferater/Profile/DevProfile.java
+++ src/main/java/unicaferater/Profile/DevProfile.java
@@ -24,59 +24,59 @@ import unicaferater.domain.Restaurant;
 
 public class DevProfile {
     
-    @Autowired 
-    private FoodRepository foodRepo;
-    @Autowired 
-    private RatingRepository ratingRepo;
-    @Autowired
-    private RestaurantRepository restaurantRepo;
-    
-    @Transactional
-    @PostConstruct
-    public void init() {
-        Food food = new Food();
-        food.setName("asd");
-        food.setPrice(Price.Kevyesti);
-        
-        Food food2 = new Food();
-        food2.setName("Makarooni");
-        food2.setPrice(Price.Maukkaasti);
-        
-        Rating rating2 = new Rating();
-        rating2.setDate(new Date());
-        rating2.setRating(3);
-        
-        rating2 = ratingRepo.save(rating2);
-        
-        List<Rating> ratings2 = new ArrayList<>();
-        ratings2.add(rating2);
-        
-        food2.setRatings(ratings2);
-        foodRepo.save(food2);
-        
-        Rating rating = new Rating();
-        rating.setDate(new Date());
-        rating.setRating(5);
-
-        rating = ratingRepo.save(rating);
-
-        List<Rating> ratings = new ArrayList<>();
-        ratings.add(rating);
-        
-        food.setRatings(ratings);
-        
-        foodRepo.save(food);
-        
-        Restaurant resta = new Restaurant();
-        resta.setName("Exactum");
-        restaurantRepo.save(resta);
-        
-        Restaurant resta2 = new Restaurant();
-        resta2.setName("Chemicum");
-        restaurantRepo.save(resta2);
-        
-        
-        
-    }
+//    @Autowired 
+//    private FoodRepository foodRepo;
+//    @Autowired 
+//    private RatingRepository ratingRepo;
+//    @Autowired
+//    private RestaurantRepository restaurantRepo;
+//    
+//    @Transactional
+//    @PostConstruct
+//    public void init() {
+//        Food food = new Food();
+//        food.setName("asd");
+//        food.setPrice(Price.Kevyesti);
+//        
+//        Food food2 = new Food();
+//        food2.setName("Makarooni");
+//        food2.setPrice(Price.Maukkaasti);
+//        
+//        Rating rating2 = new Rating();
+//        rating2.setDate(new Date());
+//        rating2.setRating(3);
+//        
+//        rating2 = ratingRepo.save(rating2);
+//        
+//        List<Rating> ratings2 = new ArrayList<>();
+//        ratings2.add(rating2);
+//        
+//        food2.setRatings(ratings2);
+//        foodRepo.save(food2);
+//        
+//        Rating rating = new Rating();
+//        rating.setDate(new Date());
+//        rating.setRating(5);
+//
+//        rating = ratingRepo.save(rating);
+//
+//        List<Rating> ratings = new ArrayList<>();
+//        ratings.add(rating);
+//        
+//        food.setRatings(ratings);
+//        
+//        foodRepo.save(food);
+//        
+//        Restaurant resta = new Restaurant();
+//        resta.setName("Exactum");
+//        restaurantRepo.save(resta);
+//        
+//        Restaurant resta2 = new Restaurant();
+//        resta2.setName("Chemicum");
+//        restaurantRepo.save(resta2);
+//        
+//        
+//        
+//    }
     
 }
