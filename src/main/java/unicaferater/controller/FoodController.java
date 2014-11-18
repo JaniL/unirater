4f74c163c285b64bd688183ee49b2305acb4a9cc
Jani Luukko <jj.luukko@gmail.com>
2014-11-19 01:06:29 +0200

    Another fix

--- src/main/java/unicaferater/controller/FoodController.java
+++ src/main/java/unicaferater/controller/FoodController.java
@@ -13,10 +13,13 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import unicaferater.Repository.FoodRepository;
+import unicaferater.Repository.RatingRepository;
 import unicaferater.Repository.RestaurantRepository;
 import unicaferater.domain.Food;
+import unicaferater.domain.Rating;
 
 /**
  *
