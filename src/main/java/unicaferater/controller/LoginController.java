84d48b386baf0e4e6c4760f92ac087403c9c7974
Jani Luukko <jj.luukko@gmail.com>
2014-12-16 20:48:18 +0200

    Uutta testausta

--- src/main/java/unicaferater/controller/LoginController.java
+++ src/main/java/unicaferater/controller/LoginController.java
@@ -13,7 +13,10 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
- 
+import unicaferater.auth.UserDetails;
+
+import java.nio.file.attribute.UserPrincipal;
+
 @Controller
 public class LoginController {
  
@@ -27,7 +30,11 @@ public class LoginController {
         System.out.println(SecurityContextHolder.getContext().getAuthentication().getCredentials());
         System.out.println(SecurityContextHolder.getContext().getAuthentication().getName());
         System.out.println(SecurityContextHolder.getContext().getAuthentication().getDetails());
-        System.out.println(SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+
+        System.out.println((UserPrincipal)SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+
+        UserDetails customUser = (UserDetails)SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        System.out.println(customUser.getId());
         return "lol";
     }
 }
